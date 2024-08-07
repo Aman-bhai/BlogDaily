e83946572dc2f3657d9c1f33ee84ffa8f20536b7
Aman Soni <amansoni18860@gmail.com>
2024-08-07 21:51:16 +0530

    resolve

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
 const bcryptjs = require('bcryptjs');
-const jwt = require("jsonwebtoken");
 const { userModel } = require('../database');
 const jwt = require("jsonwebtoken");
 
