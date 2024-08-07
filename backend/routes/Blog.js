3d4656d8e76a1c5d186d06ee123cd4a3d3820f3d
Aman Soni <amansoni18860@gmail.com>
2024-08-07 21:54:08 +0530

    resolve issue

--- backend/routes/Blog.js
+++ backend/routes/Blog.js
@@ -3,7 +3,29 @@ const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const mongoose = require('mongoose');
 const { Blogmodel } = require("../database");
-const fetchuser = require("../middleware/fetchuser");
+
+const jwt = require("jsonwebtoken");
+
+const jwtSecret = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, laudantium!";
+
+const fetchuser = (req, res, next) => {
+    const token = req.header("auth-token");
+    console.log("Auth Token:success");
+
+    if (!token) {
+        return res.status(401).json({ error: "Please authenticate using a valid token" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        req.user = decoded;
+        next();
+
+    } catch (error) {
+        console.error("Token verification error:", error);
+        res.status(401).json({ error: "Please authenticate using a valid token" });
+    }
+};
 
 // Route 1: Get all blogs (Login required)
 router.get("/fetchallblogs", fetchuser, async (req, res) => {
